afd9f9982d9231a3007f6c6816ddd77318aa2584
ajlveloper <ajveloper@gmail.com>
2022-02-16 10:53:48 -0400

    actualizacion

--- src/components/ViewPackaje/OrderComplete/OrderComplete.jsx
+++ src/components/ViewPackaje/OrderComplete/OrderComplete.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Delivery from "../../../assets/img/Delivery";
 import './OrderComplete.css'
 const OrderComplete = () => {
+
+  const { nombre } = JSON.parse(localStorage.getItem('Orden')) || {};
   return (
     <div className="content-complete">
       <div className="content-delivery">
         <Delivery className="empty" />
       </div>
       <h2 className="orange title-delivery">
-        Su solicitud esta en proceso, en minutos el repartidor llegará a su hogar.
+       Hola { nombre }, su solicitud esta en proceso, en minutos el repartidor llegará a su hogar.
       </h2>
     </div>
   );
